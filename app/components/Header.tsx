17748446b42ec857974972252c9a71712373f0cd
dan bustamante <183784817+notreallydan@users.noreply.github.com>
2024-09-29 23:28:08 +1000

    many changes

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="space-y-2">
+      <h1 className="text-xl font-light">i&apos;m dan</h1>
+      <p className="text-xl font-light text-stone-500">this is a space for my work and thoughts</p>
+    </header>
+  );
+}
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,22 @@ import Section from './components/Section';
 
 export default function Home() {
   const workLinks = [
-    { href: "#", text: "misc" },
-    { href: "#", text: "muse" },
-    { href: "#", text: "loop" },
+    { href: "#", text: "rip algorithm" },
+    { href: "#", text: "journalling app" },
   ];
 
   const wordsLinks = [
-    { href: "#", text: "a love letter to SVGs" },
-    { href: "#", text: "in a year" },
-    { href: "#", text: "you might as well enjoy it" },
+    { href: "#", text: "coming soon" },
   ];
 
   return (
-    <div className="min-h-screen bg-[#e8e3e0] text-[#3c3c3c] flex items-center justify-center p-4">
-      <div className="max-w-2xl w-full space-y-16">
-        <Header />
-        <Section title="work" links={workLinks} />
-        <Section title="words" links={wordsLinks} />
+    <div className="min-h-screen bg-[#e8e3e0] text-[#3c3c3c] flex justify-center p-4">
+      <div className="max-w-2xl w-full mt-24 space-y-16">
+        <div className="mb-32">
+          <Header />
+        </div>
+        <Section title="projects" links={workLinks} />
+        <Section title="writing" links={wordsLinks} />
       </div>
     </div>
   );
